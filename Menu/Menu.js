9b0aa0f926b0593a5b362b93da3b6191184e1a73
Tim Dill <brodt7258@gmail.com>
2019-04-15 02:44:36 -0400

    close menu by clicking off it.

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -13,12 +13,24 @@
 // Using your menuButton reference, add a click handler that calls toggleMenu
 
 // This is the Stretch version.  Previous commits have the regular solution.
+const menuHandler = (() => {
+  const menuShift = TweenLite.to('.menu', 0.5, { left: -50, paused: true });
+  const modalShift = TweenLite.to('.menu-modal', 0.5, { display: 'block', opacity: 1, paused: true });
+  let open = false;
+  return () => {
+    if (open) {
+      menuShift.reverse()
+      modalShift.reverse();
+    } else {
+      menuShift.play();
+      modalShift.play();
+    }
+    open = !open;
+  };
+})();
+
 document.body.querySelector('.menu-button')
-  .addEventListener('click', (() => {
-    const menuShift = TweenLite.to('.menu', 0.5, { left: -50, paused: true });
-    let open = false;
-    return () => {
-      open = !open;
-      open ? menuShift.reverse() : menuShift.play(); 
-    };
-  })());
+  .addEventListener('click', menuHandler);
+
+document.body.querySelector('.menu-modal')
+  .addEventListener('click', menuHandler);
