fec7000c6148cbeb195d3ce35723219056060729
Tim Dill <brodt7258@gmail.com>
2019-04-14 20:11:52 -0400

    added article.  MVP

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,5 +1,5 @@
 
-const toggleMenu = (menu) => () => {
+const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence.
   menu.classList.toggle('menu--open'); 
 }
@@ -9,4 +9,4 @@ const menu = document.body.querySelector('.menu');
 // create a reference to the ".menu-button" class
 const menuButton = document.body.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
-menuButton.addEventListener('click', toggleMenu(menu));
+menuButton.addEventListener('click', toggleMenu);
