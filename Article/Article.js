eeb36e1229c3258646787ce87b43c4239b1f5079
Tim Dill <brodt7258@gmail.com>
2019-04-16 21:07:32 -0400

    cleaned up irrelevant comments

--- Article/Article.js
+++ Article/Article.js
@@ -1,27 +1,18 @@
-// Because classes are not hoisted you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
-
 class Article {
   constructor(domElement) {
-    // assign this.domElement to the passed in domElement
     this.domElement = domElement;
-    // create a reference to the ".expandButton" class. 
+
     this.expandButton = this.domElement.querySelector('.expandButton');
-    // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.textContent = 'Click to Expand';
-    // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener('click', () => this.expandArticle());
 
     this.closeButton = this.domElement.querySelector('.close');
     this.closeButton.addEventListener('click', () => this.deleteArticle());
 
     this.open = false;
-    
   }
   
   expandArticle () {
-    // Using our reference to the domElement, toggle a class to expand or hide the article.
-    //this.domElement.classList.toggle('article-open');
-
     // I first tried doing this as an IIFE with a toggle variable in the closure.  Kept losing its 'this' binding
     // Nothing I tried fixed that.  So I gave up and went with a class property.
     const article = this.domElement.querySelector('.article-content');
@@ -35,13 +26,12 @@ class Article {
     }
     this.open = !this.open;
 
-    this.closeButton.style.cssText = "display: initial"; // The content has been view at least once?  Offer the delete button
+    this.closeButton.style.cssText = "display: initial"; // The content has been viewed at least once?  Offer the delete button
   }    
 
   deleteArticle() {
     
-    TweenLite.to(this.domElement, 0.3, // Animate the article's removal from the page
-      { 
+    TweenLite.to(this.domElement, 0.3, { // Animate the article's removal from the page
         width: '25%',
         margin: 'auto',
         height: 0,
@@ -54,16 +44,5 @@ class Article {
   }
 }
 
-
-
-/* START HERE: 
-
-- Select all classes named ".article" and assign that value to the articles variable.  
-
-- With your selection in place, now chain .forEach() on to the articles variable to iterate over the articles NodeList and create a new instance of Article by passing in each article as a parameter to the Article class.
-
-*/
 document.body.querySelectorAll('.article')
   .forEach(e => new Article(e));
-// let articles;
-
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,17 +1,3 @@
-
-// const toggleMenu = () => {
-//   // Toggle the "menu--open" class on your menu refence.
-//   //menu.classList.toggle('menu--open');
-//   console.log(menu);
-//   TweenLite.to('.menu', 1, { opacity: 0, y: 200 });
-// }
-
-// // Start Here: Create a reference to the ".menu" class
-// const menu = document.body.querySelector('.menu');
-// // create a reference to the ".menu-button" class
-
-// Using your menuButton reference, add a click handler that calls toggleMenu
-
 // This is the Stretch version.  Previous commits have the regular solution.
 const menuHandler = (() => {
   const menuShift = TweenLite.to('.menu', 0.5, { left: -50, paused: true });
